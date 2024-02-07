a9ae774cd8346221bc6ab474287d22b2b2ac1edb
Ma Chi <hellomarch@gmail.com>
2024-02-07 08:19:05 +0100

    transaction is reserved by pg, so rename the db

--- infrastructure/aws/db.tf
+++ infrastructure/aws/db.tf
@@ -1,11 +1,11 @@
 resource "aws_db_instance" "db" {
-  db_name              = "${var.app_name}"
+  db_name              = "db${var.app_name}"
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "postgres"
   engine_version       = "16.1"
   instance_class       = "db.t4g.small"
-  username             = "${var.app_name}"
+  username             = "db${var.app_name}"
   password             = random_password.password.result
   parameter_group_name = "default.postgres16"
   skip_final_snapshot  = true
